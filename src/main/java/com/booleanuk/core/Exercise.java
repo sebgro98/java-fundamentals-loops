3fb0ca082a1e5c66aff4e4d6a285549b3397adc6
Sebastian <sebgro@kth.se>
2024-08-12 20:56:25 +0200

    Done with both

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -1,9 +1,9 @@
 package com.booleanuk.core;
 
 public class Exercise {
-    public int[] numsZeroToThree;
-    public int[] numsFiveToTen;
-    public int[] countdown;
+    public int[] numsZeroToThree = new int[4];
+    public int[] numsFiveToTen = new int[6];
+    public int[] countdown = new int [4];
 
     public int[] favouriteNumbers = {1, 2, 4, 5, 7, 8, 10};
 
@@ -11,25 +11,35 @@ public class Exercise {
 
     public void stepOne() {
         // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
-
-
+        for(int i = 0; i < 4; i++) {
+            numsZeroToThree[i] = i;
+        }
     }
 
     public void stepTwo() {
         // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
-
+        for(int i = 0; i < numsFiveToTen.length; i++) {
+            numsFiveToTen[i] = i + 5;
+        }
 
     }
 
     public void stepThree() {
         // TODO: 3. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
-
+        int k = 3;
+        for(int i = 3; i > 0; i--) {
+            countdown[k - i] = i ;
+        }
 
     }
 
     public boolean stepFour(int num) {
         // TODO: 6. Write a for loop that checks if num is in the favouriteNumbers array
-
+        for(int i = 0; i < favouriteNumbers.length; i++) {
+            if(num == favouriteNumbers[i]) {
+                return true;
+            }
+        }
 
 
         return false;
@@ -37,7 +47,11 @@ public class Exercise {
 
     public boolean stepFive(String hobby) {
         // TODO 5. Write a for loop that checks if the hobby String is in the myHobbies array
-
+        for(int i = 0; i < myHobbies.length; i++) {
+            if(hobby.equals(myHobbies[i])) {
+                return true;
+            }
+        }
 
 
         return false;
--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -1,11 +1,17 @@
 package com.booleanuk.extension;
 
 public class Extension {
-    public int[] numsEven;
+    public int[] numsEven = new int[4];
 
     public void stepOne() {
         // TODO: 1. Write a for loop that adds all the even numbers between 0 and 6 (0, 2, 4, 6) to evenNums
-
+        int k = 0;
+        for (int i = 0; i < 7; i++) {
+            if(i % 2 == 0) {
+                numsEven[k] = i;
+                k++;
+            }
+        }
 
         
     }
